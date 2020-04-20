6dfd0ffbf99e6c65195ec49a1180f66191806210
Mahesh Deshwal <m.deshwal93@gmail.com>
2020-04-20 10:57:21 +0530

    like,unlike

--- insta_methods.py
+++ insta_methods.py
@@ -7,6 +7,13 @@ from random import randint
 import os
 
 
+def change_ip():
+    os.system("ipconfig /release")
+    time.sleep(3)
+    os.system("ipconfig /renew")
+    time.sleep(3)
+
+
 def create_session():
     br=webdriver.Chrome()
     url = 'https://www.instagram.com/accounts/login/'
@@ -37,5 +44,24 @@ def get_famous():
     return famous
 
 
+def open_random_image(br):
+    imgs = br.find_elements_by_class_name('_9AhH0')
+    imgs[random.randint(0,len(imgs))].click()
 
 
+def like_image(br,user_name):
+    like_xpath = '/html/body/div[4]/div[2]/div/article/div[2]/section[1]/span[1]/button'
+    like_button = br.find_element_by_xpath(like_xpath)
+    svg_css = 'body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.FY9nT.fr66n > button > svg'
+    if br.find_element_by_css_selector(svg_css).get_attribute('aria-label') == 'Like':
+        like_button.click()
+        with open ('liked.txt','a') as f:
+            f.write('%s\n'%user_name)
+
+
+def unlike_image(br):
+    like_xpath = '/html/body/div[4]/div[2]/div/article/div[2]/section[1]/span[1]/button'
+    like_button = br.find_element_by_xpath(like_xpath)
+    svg_css = 'body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.FY9nT.fr66n > button > svg'
+    if br.find_element_by_css_selector(svg_css).get_attribute('aria-label') == 'Unlike':
+        like_button.click()
